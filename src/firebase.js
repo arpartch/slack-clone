43c939954f87ae4af535e9c006c89a7ea5e7aeb4
Athena <athena.partch@gmail.com>
2021-06-05 09:25:12 -0700

    add firebase and styled components

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAZs0thxnEVZSOp3OAoYaw7dVsdbgafCQ4",
+    authDomain: "slack-clone-29aaf.firebaseapp.com",
+    projectId: "slack-clone-29aaf",
+    storageBucket: "slack-clone-29aaf.appspot.com",
+    messagingSenderId: "542945399871",
+    appId: "1:542945399871:web:360e3cdb6e07940a10548a",
+    measurementId: "G-63JKJEB9PT"
+  };
