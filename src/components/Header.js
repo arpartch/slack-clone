2278ad1a513f9d4f477bdc7dc42b3605ad6cfd76
Athena <athena.partch@gmail.com>
2021-06-07 19:07:31 -0700

    add color to nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from "styled-components";
 import { Avatar } from "@material-ui/core";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
 
 function Header() {
     return <HeaderContainer>
@@ -9,6 +10,7 @@ function Header() {
             <HeaderAvatar
                 // TODO: Add onclick
             />
+            <AccessTimeIcon />
         </HeaderLeft>
 
         {/* Header Search */}
@@ -19,8 +21,33 @@ function Header() {
 
 export default Header
 
-const HeaderContainer = styled.div``;
+const HeaderContainer = styled.div`
+    display: flex;
+    position: fixed;
+    width: 100%;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 0;
+    background-color: var(--slack-color);
+    color: white;
+`;
 
-const HeaderLeft = styled.div``;
+const HeaderLeft = styled.div`
+    flex: 0.3;
+    display: flex;
+    align-items: center;
+    margin-left: 20px;
 
-const HeaderAvatar = styled(Avatar)``;
+    > .MuiSvgIcon-root {
+        margin-left: auto;
+        margin-right: 30px;
+    }
+`;
+
+const HeaderAvatar = styled(Avatar)`
+    cursor: pointer;
+
+    :hover {
+        opacity: 0.8;
+    }
+`;
