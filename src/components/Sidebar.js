ac40859e3859fae222bee19005490c056b14c215
Athena <athena.partch@gmail.com>
2021-06-08 18:17:42 -0700

    add database cloud firestone

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 import CreateIcon from "@material-ui/icons/Create"
 import SidebarOption from './SidebarOption';
-import { Apps, BookmarkBorder, Drafts, ExpandLess, FileCopy, Inbox, InsertComment, PeopleAlt } from '@material-ui/icons';
+import { Add, Apps, BookmarkBorder, Drafts, ExpandLess, ExpandMore, FileCopy, Inbox, InsertComment, PeopleAlt } from '@material-ui/icons';
 
 function Sidebar() {
     return (
@@ -27,7 +27,10 @@ function Sidebar() {
             <SidebarOption Icon={Apps} title="Apps"/>
             <SidebarOption Icon={FileCopy} title="File browser"/>
             <SidebarOption Icon={ExpandLess} title="Show less"/>
-
+            <hr />
+            <SidebarOption Icon={ExpandMore} title="Channels"/>
+            <hr />
+            <SidebarOption Icon={Add} addChannelOption title="Add Channel"/>
 
         </SidebarContainer>
     )
@@ -43,6 +46,12 @@ const SidebarContainer = styled.div`
     border-top: 1px solid #49274b;
     max-width: 260px;
     margin-top: 60px;
+
+    > hr {
+        margin-top: 10px;
+        margin-bottom: 10px;
+        border: 1px solid #49274b
+    }
 `;
 
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAZs0thxnEVZSOp3OAoYaw7dVsdbgafCQ4",
@@ -8,3 +10,10 @@ const firebaseConfig = {
     appId: "1:542945399871:web:360e3cdb6e07940a10548a",
     measurementId: "G-63JKJEB9PT"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider, db }; 
